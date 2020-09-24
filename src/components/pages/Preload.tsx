4d1ba7ae103e64b1024dc6469130f559deecd4b6
Filipe de Castro <filipeyoga@gmail.com>
2020-09-24 20:11:09 -0300

    feat: updating main pages

--- src/components/pages/Preload.tsx
+++ src/components/pages/Preload.tsx
@@ -1,9 +1,23 @@
-import React, { ReactElement } from 'react';
-import { Text } from 'react-native';
+import React, { ReactElement, useEffect, useState } from 'react';
+import styled from 'styled-components/native';
 import Container from '../templates/Container';
+import theme from '../../theme/nice';
+import { useNavigation } from '@react-navigation/native';
 
-export default (): ReactElement => (
-  <Container>
-    <Text>Preload</Text>
-  </Container>
-);
+export default (): ReactElement => {
+  const navigation = useNavigation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000);
+    !loading && navigation.navigate('SignIn');
+  }, [loading]);
+
+  return <Container>{loading && <Loading>Loading...</Loading>}</Container>;
+};
+
+const Loading = styled.Text`
+  color: ${theme.colors.secondary[100]};
+  font-family: ${theme.font.family.bold};
+  font-size: ${theme.sizes[400]};
+`;
--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -1,9 +1,20 @@
 import React, { ReactElement } from 'react';
 import { Text } from 'react-native';
+import styled from 'styled-components/native';
 import Container from '../templates/Container';
+import theme from '../../theme/nice';
 
-export default (): ReactElement => (
-  <Container>
-    <Text>SignIn</Text>
-  </Container>
-);
+export default (): ReactElement => {
+  const [showLoginBox, setShowLoginBox] = 
+  return (
+    <Container>
+      <Title onPress={setShowLoginBox(true)}>Sign In</Title>
+    </Container>
+  );
+};
+
+const Title = styled.Text`
+  color: ${theme.colors.secondary[100]};
+  font-family: ${theme.font.family.bold};
+  font-size: ${theme.sizes[400]};
+`;
